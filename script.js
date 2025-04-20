1a162fb8fde4a583f9840803c5dd9fb0c43ee9fd
BaderOth <112273605+BaderOth@users.noreply.github.com>
2025-04-20 18:55:03 +0300

    Delete Files

--- script.js
+++
@@ -1,9 +0,0 @@
-const navToggle = document.getElementById('navToggle');
-const navLinks = document.getElementById('navLinks');
-const navClose = document.getElementById('navClose');
-navToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-});
-navClose.addEventListener('click', () => {
-    navLinks.classList.remove('open');
-});
